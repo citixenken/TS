cad9057da663aa527eefc54bbcb4bae078babd33
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-14 12:08:19 +0300

    intro

---
+++ build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = "citixenken";
+console.log(username);
+let a = 12;
+let b = 60;
+let c = 1;
+console.log(a / b);
+console.log(c * b);
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+let username = "citixenken";
+console.log(username);
+
+let a: number = 12;
+let b: number = 60;
+let c: number = 1;
+
+console.log(a / b);
+
+console.log(c * b);
