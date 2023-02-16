7bc08642b75e9a88c6dc25970c312e880d8fcb8f
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-16 13:13:36 +0300

    arrays and objects

--- build/js/main.js
+++ build/js/main.js
@@ -1,21 +1,50 @@
 "use strict";
-let myName = "citixenken";
-let meaningOfLife;
-let isLoading;
-let album;
-//union type
-let track;
-let isActive;
-// regex
-let re;
-myName = "John Wick";
-meaningOfLife = 42;
-isLoading = false;
-album = true;
-track = 1;
-isActive = 0;
-re = /\w+/g;
-const sum = (a, b) => {
-    return a + b;
+let stringArr;
+let planets;
+let mixedData;
+let test = [];
+let bands = [];
+stringArr = ["Alpha", "Beta", "Gamma"];
+planets = ["Mars", 3, "Saturn"];
+mixedData = ["CVT", 1984, false];
+planets = stringArr;
+bands.unshift("Metallica");
+//tuple
+let myTuple = ["Oaxaca", 9, false];
+let mixedArray = ["Jim", 7, true];
+mixedArray = myTuple;
+// myTuple = mixedArray; ❌
+// objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = stringArr;
+myObj = {};
+const userData = {
+    name: "John",
+    age: 16,
 };
-console.log(sum(6, 12));
+let evh;
+let jp;
+evh = { name: "Eddie van Halen", active: false, albums: ["OU812", 1984, 5150] };
+jp = { albums: ["I", "II", "IV"] };
+evh = jp;
+const welcomeGuitarist = (guitarist) => {
+    //   return `Welcome, ${guitarist.name?.toUpperCase()}!`;
+    if (guitarist.name) {
+        return `Welcome, ${guitarist.name.toUpperCase()}!`;
+    }
+    return "Hello!";
+};
+console.log(welcomeGuitarist(evh));
+// enums
+// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime";
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.D);
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,81 @@
-let myName: string = "citixenken";
-let meaningOfLife: number;
-let isLoading: boolean;
-let album: any;
+let stringArr: string[];
 
-//union type
-let track: string | number;
-let isActive: number | boolean | string;
+let planets: (string | number)[];
 
-// regex
-let re: RegExp;
+let mixedData: (string | boolean | number)[];
 
-myName = "John Wick";
-meaningOfLife = 42;
-isLoading = false;
-album = true;
-track = 1;
-isActive = 0;
-re = /\w+/g;
+let test = [];
 
-const sum = (a: number, b: number) => {
-  return a + b;
+let bands: string[] = [];
+
+stringArr = ["Alpha", "Beta", "Gamma"];
+planets = ["Mars", 3, "Saturn"];
+mixedData = ["CVT", 1984, false];
+
+planets = stringArr;
+bands.unshift("Metallica");
+
+//tuple
+let myTuple: [string, number, boolean] = ["Oaxaca", 9, false];
+
+let mixedArray = ["Jim", 7, true];
+
+mixedArray = myTuple;
+
+// myTuple = mixedArray; ❌
+
+// objects
+let myObj: object;
+myObj = [];
+console.log(typeof myObj);
+myObj = stringArr;
+myObj = {};
+
+const userData = {
+  name: "John",
+  age: 16,
 };
 
-console.log(sum(6, 12));
+// type v interface
+// type Guitarist = {
+//   name: string;
+//   active?: boolean;
+//   albums: (string | number)[];
+// };
+
+interface Guitarist {
+  name?: string;
+  active?: boolean;
+  albums?: (string | number)[];
+}
+
+let evh: Guitarist;
+let jp: Guitarist;
+
+evh = { name: "Eddie van Halen", active: false, albums: ["OU812", 1984, 5150] };
+jp = { albums: ["I", "II", "IV"] };
+
+evh = jp;
+
+const welcomeGuitarist = (guitarist: Guitarist) => {
+  //   return `Welcome, ${guitarist.name?.toUpperCase()}!`;
+  if (guitarist.name) {
+    return `Welcome, ${guitarist.name.toUpperCase()}!`;
+  }
+  return "Hello!";
+};
+
+console.log(welcomeGuitarist(evh));
+
+// enums
+// "Unlike most TypeScript features, Enums are not a type-level addition to JavaScript but something added to the language and runtime";
+
+enum Grade {
+  U = 1,
+  D,
+  C,
+  B,
+  A,
+}
+
+console.log(Grade.D);
