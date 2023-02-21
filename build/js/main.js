1848f0fa71d3399998426057223fc60e12a10323
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-21 17:47:17 +0300

    utility types

--- build/js/main.js
+++ build/js/main.js
@@ -1,12 +1,36 @@
 "use strict";
-const echo = (arg) => arg;
-// console.log(echo(42));
-const isObj = (arg) => {
-    return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
+// partial
+const updateAssignment = (assign, propsToUpdate) => {
+    return Object.assign(Object.assign({}, assign), propsToUpdate);
 };
-console.log(isObj(true)); //false
-console.log(isObj("John")); //false
-console.log(isObj([7, 69, 420])); //false
-console.log(isObj({ name: "Saf" })); //true
-console.log(isObj(null)); //false
-/***************************************************************************/
+const assign1 = {
+    studentId: "compsco069",
+    title: "Final Project",
+    grade: 0,
+};
+console.log(updateAssignment(assign1, { grade: 69 }));
+const assignGraded = updateAssignment(assign1, { grade: 69 });
+// required and readonly
+const recordAssignment = (assign) => {
+    // send to DB
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: true });
+//record
+const hexColorMap = {
+    red: "FF0000",
+    green: "00FF00",
+    blue: "0000FF",
+};
+const finalGrades = {
+    Sara: "U",
+    Kelly: "B",
+};
+const score = {
+    studentId: "Maverick",
+    grade: 76,
+};
+const preview = {
+    studentId: "Maverick02",
+    title: "End of Semester Project",
+};
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,74 @@
-const echo = <T>(arg: T): T => arg;
+// partial
 
-// console.log(echo(42));
+interface Assignment {
+  studentId: string;
+  title: string;
+  grade: number;
+  verified?: boolean;
+}
 
-const isObj = <T>(arg: T) => {
-  return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
+const updateAssignment = (
+  assign: Assignment,
+  propsToUpdate: Partial<Assignment>
+): Assignment => {
+  return { ...assign, ...propsToUpdate };
 };
 
-console.log(isObj(true)); //false
-console.log(isObj("John")); //false
-console.log(isObj([7, 69, 420])); //false
-console.log(isObj({ name: "Saf" })); //true
-console.log(isObj(null)); //false
+const assign1: Assignment = {
+  studentId: "compsco069",
+  title: "Final Project",
+  grade: 0,
+};
 
-/***************************************************************************/
+console.log(updateAssignment(assign1, { grade: 69 }));
+const assignGraded: Assignment = updateAssignment(assign1, { grade: 69 });
+
+// required and readonly
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+  // send to DB
+  return assign;
+};
+const assignVerified: Readonly<Assignment> = {
+  ...assignGraded,
+  verified: true,
+};
+
+//record
+const hexColorMap: Record<string, string> = {
+  red: "FF0000",
+  green: "00FF00",
+  blue: "0000FF",
+};
+
+type Students = "Sara" | "Kelly";
+
+type LetterGrades = "A" | "B" | "C" | "D" | "U";
+
+const finalGrades: Record<Students, LetterGrades> = {
+  Sara: "U",
+  Kelly: "B",
+};
+
+// pick and omit
+
+type AssignResult = Pick<Assignment, "studentId" | "grade">;
+
+const score: AssignResult = {
+  studentId: "Maverick",
+  grade: 76,
+};
+type AssignPreview = Omit<Assignment, "grade" | "verified">;
+
+const preview: AssignPreview = {
+  studentId: "Maverick02",
+  title: "End of Semester Project",
+};
+
+// exclude and extract
+type adjustedGrade = Exclude<LetterGrades, "U">;
+type highGrades = Extract<LetterGrades, "A" | "B">;
+
+// Non-nullable
+type AllPossibleUsers = "Monty" | "Sienna" | null | undefined;
+
+type NamesOnly = NonNullable<AllPossibleUsers>;
