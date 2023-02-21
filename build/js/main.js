0ddf7c0d1d6b51059164cbf08206bb69e3026686
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-21 14:45:02 +0300

    generics

--- build/js/main.js
+++ build/js/main.js
@@ -1,52 +1,12 @@
 "use strict";
-// interface TransactionObj {
-//   readonly [index: string]: number;
-// }
-const todaysTransactions = {
-    Pizza: -10,
-    Books: -5,
-    Job: 50,
+const echo = (arg) => arg;
+// console.log(echo(42));
+const isObj = (arg) => {
+    return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
 };
-// console.log(`Accessibility: ${(todaysTransactions.Books = 40)}`);
-console.log(`Accessibility: ${todaysTransactions.Books}`);
-let prop = "Books";
-console.log(`Using index signatures: ${todaysTransactions[prop]}`);
-const todaysNet = (transactions) => {
-    let total = 0;
-    for (const transaction in transactions) {
-        total += transactions[transaction];
-    }
-    return total;
-};
-console.log(todaysNet(todaysTransactions));
-console.log(todaysTransactions["Jeff"]); //undefined
-const student = {
-    name: "Duke",
-    GPA: 3.9,
-    classes: [150, 200],
-};
-// console.log(student.grade);
-// for (const key in student) {
-//   console.log(`${key}: ${student[key]}`);
-// }
-////////////////////////////////////////////////////////////////
-for (const key in student) {
-    console.log(`${key}: ${student[key]}`);
-}
-////////////////////////////////////////////////////////////////
-Object.keys(student).map((key) => {
-    console.log(student[key]);
-});
-////////////////////////////////////////////////////////////////
-const logStudentKey = (student, key) => {
-    console.log(`Student ${key}: ${student[key]}`);
-};
-logStudentKey(student, "name");
-const monthlyIncomes = {
-    salary: 420,
-    bonus: 69,
-    sidehustle: 1774,
-};
-for (const revenue in monthlyIncomes) {
-    console.log(monthlyIncomes[revenue]);
-}
+console.log(isObj(true)); //false
+console.log(isObj("John")); //false
+console.log(isObj([7, 69, 420])); //false
+console.log(isObj({ name: "Saf" })); //true
+console.log(isObj(null)); //false
+/***************************************************************************/
--- src/main.ts
+++ src/main.ts
@@ -1,93 +1,15 @@
-// index signatures
-interface TransactionObj {
-  readonly [index: string]: number;
+const echo = <T>(arg: T): T => arg;
 
-  Pizza: number;
-  Books: number;
-  Job: number;
-}
+// console.log(echo(42));
 
-// interface TransactionObj {
-//   readonly [index: string]: number;
-// }
-
-const todaysTransactions: TransactionObj = {
-  Pizza: -10,
-  Books: -5,
-  Job: 50,
+const isObj = <T>(arg: T) => {
+  return typeof arg === "object" && !Array.isArray(arg) && arg !== null;
 };
 
-// console.log(`Accessibility: ${(todaysTransactions.Books = 40)}`);
-console.log(`Accessibility: ${todaysTransactions.Books}`);
+console.log(isObj(true)); //false
+console.log(isObj("John")); //false
+console.log(isObj([7, 69, 420])); //false
+console.log(isObj({ name: "Saf" })); //true
+console.log(isObj(null)); //false
 
-let prop: string = "Books";
-
-console.log(`Using index signatures: ${todaysTransactions[prop]}`);
-
-const todaysNet = (transactions: TransactionObj): number => {
-  let total = 0;
-  for (const transaction in transactions) {
-    total += transactions[transaction];
-  }
-  return total;
-};
-
-console.log(todaysNet(todaysTransactions));
-console.log(todaysTransactions["Jeff"]); //undefined
-
-interface Student {
-  // [key: string]: any;
-  // [key: string]: string | number | number[] | undefined;
-  name: string;
-  GPA: number;
-  classes?: number[];
-}
-
-const student: Student = {
-  name: "Duke",
-  GPA: 3.9,
-  classes: [150, 200],
-};
-
-// console.log(student.grade);
-
-// for (const key in student) {
-//   console.log(`${key}: ${student[key]}`);
-// }
-
-////////////////////////////////////////////////////////////////
-
-for (const key in student) {
-  console.log(`${key}: ${student[key as keyof Student]}`);
-}
-////////////////////////////////////////////////////////////////
-
-Object.keys(student).map((key) => {
-  console.log(student[key as keyof typeof student]);
-});
-////////////////////////////////////////////////////////////////
-
-const logStudentKey = (student: Student, key: keyof Student): void => {
-  console.log(`Student ${key}: ${student[key]}`);
-};
-
-logStudentKey(student, "name");
-////////////////////////////////////////////////////////////////
-
-// interface Incomes {
-//   [key: string]: number;
-// }
-
-type Streams = "salary" | "bonus" | "sidehustle";
-
-type Incomes = Record<Streams, number>;
-
-const monthlyIncomes: Incomes = {
-  salary: 420,
-  bonus: 69,
-  sidehustle: 1774,
-};
-
-for (const revenue in monthlyIncomes) {
-  console.log(monthlyIncomes[revenue as keyof Incomes]);
-}
+/***************************************************************************/
